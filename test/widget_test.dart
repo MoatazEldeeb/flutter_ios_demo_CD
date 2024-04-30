232ce11437f55123694028308f69aa9eef0e8a6b
MoatazEldeeb <moatazdx1@gmail.com>
2024-04-30 21:39:57 +0300

    init2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:counter_demo/main.dart';
+import 'package:live_object_detection_ssd_mobilenet/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
